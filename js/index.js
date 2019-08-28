81d176b2345182abe2e44cc5ae5633ea37915806
lyb0841 <469276764@qq.com>
2019-08-28 14:55:37 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
-// document.querySelector('.nav-btn').onclick=function(){
-//     const navDom = document.querySelector('.nav-item-container')
-//     if(navDom.className.indexOf('active') === -1){
-//         navDom.className = `nav-item-container active`
-//     }else{
-//         navDom.className = `nav-item-container`
-//     }
-// }
+var navBtn = document.querySelector('.nav-btn');
+console.log(navBtn)
+if(navBtn){
+    document.querySelector('.nav-btn').onclick=function(){
+        const navDom = document.querySelector('.nav-item-container')
+        if(navDom.className.indexOf('active') === -1){
+            navDom.className = `nav-item-container active`
+        }else{
+            navDom.className = `nav-item-container`
+        }
+    }
+}
